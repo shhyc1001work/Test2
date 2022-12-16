201c51f5c01201c328e0bab045d9f7f42a28726f
Indirect91 <shhyc1001@gmail.com>
2022-12-16 11:46:53 +0900

    Added script
    인트값으로 100을 넣었다

---
+++ Assets/Test3.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test3 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int test4;
+        test4 = 100;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
